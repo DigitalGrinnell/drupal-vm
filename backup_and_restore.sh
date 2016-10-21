1176c1447e3e50f494a15463137df1f1593e04bb
Mark McFate <mcfatem@grinnell.edu>
2016-10-21 15:24:58 -0500

    Including Bazil's backup_and_restore.sh

---
+++ backup_and_restore.sh
@@ -0,0 +1,10 @@
+alias vm_create_backup="ssh vagrant@rootstalk.dev \"cd /var/www/drupal/web/sites/default;drush ard default --no-core --destination='./rootstalk.tar.gz' --overwrite\""  
+alias rsync_file_message="printf \"Transfering backup to '/Volumes/RS' directory on your local machine: \n\""
+alias backup_complete_message="printf \"File backup complete!!! These are the contents of the flash drive...\n\""
+alias vm_pull_to_RS="rsync_file_message;rsync root@rootstalk.dev:/var/www/drupal/web/rootstalk.tar.gz /Volumes/RS/;backup_complete_message;ls /Volumes/RS;"
+alias vm_save_to_shared="ssh root@rootstalk.dev \"cp /var/www/drupal/web/rootstalk.tar.gz /var/www/drupalvm\""
+alias rootstalk_vm_backup="vm_create_backup;vm_save_to_shared;vm_pull_to_RS"
+
+alias vm_push_to_vm="rsync ./rootstalk.tar.gz vagrant@rootstalk.dev:/var/www/drupal/web/"
+alias vm_restore_backup="ssh root@rootstalk.dev \"cd /var/www/drupal; drush arr -v web/rootstalk.tar.gz default --overwrite --tar-options='vz';cd web/sites/default; drush cr all\""
+alias rootstalk_vm_restore="vm_push_to_vm;vm_restore_backup"
